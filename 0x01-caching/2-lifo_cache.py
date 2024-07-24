598879712a4036b76cb7bc5718d3460330310e9b
kirlos basta <kirlosbasta@gmail.com>
2024-07-25 02:36:24 +0300

    Add 2-lifo_cache.py

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,31 +1,30 @@
 #!/usr/bin/env python3
-'''1. FIFO caching'''
-from collections import deque
+'''2. LIFO Caching'''
 
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class FIFOCache(BaseCaching):
+class LIFOCache(BaseCaching):
     '''
-    class FIFOCache that inherits from BaseCaching and is a
-    caching system which implement first in first out algorithm
+    class LIFOCache that inherits from BaseCaching and is a
+    caching system which implement last in first out algorithm
     '''
     def __init__(self):
         '''initiate an instance'''
         super().__init__()
-        self.qeue = deque()
+        self.stack = list()
 
     def put(self, key, item):
-        '''Assigin the value of item to key using FIFO Algorithm'''
+        '''Assigin the value of item to key using LIFO Algorithm'''
         if key is None or item is None:
             return
         if len(self.cache_data.keys()) >= self.MAX_ITEMS\
                 and key not in self.cache_data:
-            old_key = self.qeue.popleft()
+            old_key = self.stack.pop()
             print('DISCARD: {}'.format(old_key))
             del self.cache_data[old_key]
-        self.qeue.append(key)
+        self.stack.append(key)
         self.cache_data[key] = item
 
     def get(self, key):
