6ffeb5e80f8be260bc0f39ad8699009113157573
kirlos basta <kirlosbasta@gmail.com>
2024-07-31 03:43:17 +0300

    Add 1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,16 +6,16 @@ from flask import Flask, render_template
 from flask_babel import Babel
 
 
-app = Flask(__name__)
-babel = Babel(app=app, default_locale='en', default_timezone='UTC')
-
-
 class Config:
     '''Basic configuration class'''
     LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-bable_config = Config()
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
 
 
 @app.route('/')
