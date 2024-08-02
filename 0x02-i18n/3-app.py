3c6ca8452edcc6df71b63fc49b663c921c0c224a
kirlos basta <kirlosbasta@gmail.com>
2024-08-02 17:08:38 +0300

    Update 3-app

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
-'''
-3. Parametrize templates
-'''
+'''3. Parametrize templates'''
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 
-class Config(object):
-    '''
-    Basic configuration class for babel format
-    '''
+class Config:
+    '''Basic configuration class for babel format'''
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -23,17 +19,13 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale() -> str:
-    '''
-    pick the best language match for each user
-    '''
+    '''pick the best language match for each user'''
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index() -> str:
-    '''
-    render a simple index.html for the user
-    '''
+    '''render a simple index.html for the user'''
     return render_template('3-index.html')
 
 
