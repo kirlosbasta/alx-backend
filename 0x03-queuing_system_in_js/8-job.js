fd3d9db113a0409df419a28885655c70e31537a2
kirlos basta <kirlosbasta@gmail.com>
2024-08-28 02:19:25 +0300

    Add 8-job.js

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,24 @@
+function createPushNotificationsJobs(jobs, queue) {
+  if (!(jobs instanceof Array)) {
+    throw new Error('Jobs is not an array');
+  }
+  for (const jobObj of jobs) {
+    const job = queue.create('push_notification_code_3', jobObj).save((err) => {
+      if (!err) {
+        console.log(`Notification job created: ${job.id}`);
+      }
+    });
+    job
+      .on('complete', () => {
+        console.log(`Notification job ${job.id} completed`);
+      })
+      .on('failed', (err) => {
+        console.log(`Notification job ${job.id} failed: ${err}`);
+      })
+      .on('progress', (progress) => {
+        console.log(`Notification job ${job.id} ${progress}% complete`);
+      });
+  }
+}
+
+export default createPushNotificationsJobs;
